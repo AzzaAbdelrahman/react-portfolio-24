ae7c2e6b9ad14dc9cd8bec6915d04f608297735c
AzzaAbdelrahman <148109380+AzzaAbdelrahman@users.noreply.github.com>
2024-07-31 16:25:05 +0100

    New

--- src/nav/Nav.js
+++ src/nav/Nav.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class Nav extends Component {
   render() {
     return (
-      <div>Thi sis the Nav Page</div>
+      <div>This is the Nav Page</div>
     )
   }
 }
